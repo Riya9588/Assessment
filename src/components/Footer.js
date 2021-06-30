cbc536ae057fc7346fe500e00d1646702a3a706a
Riya9588 <Riya9588@github.com>
2021-06-30 13:39:54 +0530

    done

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => (
+  <div className="footer">
+    <p>This is a footer</p>
+  </div>
+);
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Navbar.css';
+function Navbar(){
+    return(
+        <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        <a href="#news">Help</a>
+      </div>
+      
+    )
+}
+export default Navbar;
